1a2818f33b4e555716250624d253ea85894d286c
bbognar <bbognar0209@gmail.com>
2025-09-04 10:58:37 +0200

    add root app

---
+++ frontend/appy.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from default_data import ensure_base_session_state
+
+ensure_base_session_state(st)
+
+pages = {
+    "Home": [
+        st.Page("Home.py", title="Home"),
+    ],
+    "Projektmenedzsment": [
+        st.Page("pages/Projects.py", title="Projektek"),
+        st.Page("pages/ProjektTipusok.py", title="Projekt Típusok"),
+        st.Page("pages/IdojarasUtemezes.py", title="Időjárás Ütemezés"),
+        st.Page("pages/Resources.py", title="Erőforrások"),
+    ],
+    "AI Asszisztensek": [
+        st.Page("pages/AnyagarAjanlatkeresAI.py", title="Anyagár Ajánlatkérés"),
+        st.Page("pages/SzerzodeskeszitesAI.py", title="Szerződéskészítés"),
+    ],
+}
+
+pg = st.navigation(pages)
+pg.run()
